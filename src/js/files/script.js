1cabd5b492752ac4adeca345fb249f9acc3aa8c8
Gregory <maklakov.grigory@gmail.com>
2022-10-02 14:58:23 +0200

    add typeAnimation

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,15 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+// Подключение type - анимация печатания текста
+import Typed from 'typed.js';
+
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings', // Тут id блока из которого берем строки для анимации
+    typeSpeed: 50, // Скорость печати
+    startDelay: 1000, // Задержка перед стартом анимации
+    backDelay: 1000, // Пауза перед удалением
+    backSpeed: 10, // Скорость удаления
+    showCursor: false, // показивать курсор
+    loop: true // Указываем, повторять ли анимацию
+  });
